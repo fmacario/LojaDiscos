5101de94690f1ab66e9897e0cae9c67e37b0da4e
marcomacedo <marcocsg@gmail.com>
2017-05-09 17:29:42 +0100

    Add disco ok! missing image

--- LojaDiscos/CriarDisco.xaml.cs
+++ LojaDiscos/CriarDisco.xaml.cs
@@ -29,6 +29,7 @@ namespace LojaDiscos
             InitializeComponent();
 
             BindComboBox(GeneroCB);
+            BindComboBoxTipo(TipoCB);
         }
         public void BindComboBox(ComboBox GeneroCB)
         {
@@ -39,7 +40,68 @@ namespace LojaDiscos
             da.Fill(ds, "Genero");
             GeneroCB.ItemsSource = ds.Tables[0].DefaultView;
             GeneroCB.DisplayMemberPath = ds.Tables[0].Columns["genero"].ToString();
-           
+
+        }
+        public void BindComboBoxTipo(ComboBox TipoCB)
+        {
+            SqlConnection conn = ConnectionHelper.GetConnection();
+
+            SqlDataAdapter da = new SqlDataAdapter("Select * FROM Tipos_Discos", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Tipos_Discos");
+            TipoCB.ItemsSource = ds.Tables[0].DefaultView;
+            TipoCB.DisplayMemberPath = ds.Tables[0].Columns["tipo"].ToString();
+
+        }
+        public void Add_disco() {
+            SqlConnection conn = ConnectionHelper.GetConnection();
+            
+            using (SqlCommand cmd = new SqlCommand("InserirDisco", conn))
+            {
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@id_disco", SqlDbType.Int).Value = codigo2.Text;
+                cmd.Parameters.Add("@titulo", SqlDbType.VarChar, 30).Value = album2.Text;
+                cmd.Parameters.Add("@preço", SqlDbType.Money).Value = preço1.Text;
+                cmd.Parameters.Add("@ano", SqlDbType.Int).Value = ano2.Text;
+                cmd.Parameters.Add("@stock", SqlDbType.Int).Value = unidades2.Text;
+                cmd.Parameters.Add("@genero", SqlDbType.VarChar, 30).Value = GeneroCB.Text;
+                cmd.Parameters.Add("@tipo", SqlDbType.VarChar, 30).Value = TipoCB.Text;
+                cmd.Parameters.Add("@artista", SqlDbType.VarChar, 30).Value = artista2.Text;
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+
+                //actualizar  stocks
+                SqlCommand c = new SqlCommand("atualizarStock", conn);
+                c.CommandType = CommandType.StoredProcedure;
+                c.Parameters.AddWithValue("@id_disco", codigo2.Text);
+                c.Parameters.AddWithValue("@update", unidades2.Text);
+                conn.Open();
+                c.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Disco inserido com sucesso", "Sucesso!");
+            }
+
+
+            /*
+            string query = "INSERT INTO Discos (id_disco, preço, stock, titulo, ano,  id_artista, id_genero, id_tipo, imagemDisco)";
+            query += " VALUES (@id_disco, @preço, @stock, @titulo, @ano, @id_artista, @id_genero, @id_tipo, @imagemDisco)";
+
+            SqlCommand myCommand = new SqlCommand(query, conn);
+            myCommand.Parameters.AddWithValue("@id_disco", codigo2.Text);
+            myCommand.Parameters.AddWithValue("@preço", preço1.Text);
+            myCommand.Parameters.AddWithValue("@titulo", preço1.Text);
+            myCommand.Parameters.AddWithValue("@stock", 1);
+            myCommand.Parameters.AddWithValue("@ano", ano2.Text);
+            myCommand.Parameters.AddWithValue("@id_artista", );
+            myCommand.Parameters.AddWithValue("@id_genero", );
+            myCommand.Parameters.AddWithValue("@id_tipo", );
+            myCommand.Parameters.AddWithValue("@imagemDisco", imgPhoto);
+            myCommand.ExecuteNonQuery();*/
         }
 
         private void btnLoad_Click(object sender, RoutedEventArgs e)
@@ -56,5 +118,9 @@ namespace LojaDiscos
 
         }
 
+        private void adicionarDisco_Click(object sender, RoutedEventArgs e)
+        {
+            Add_disco();
+        }
     }
 }
