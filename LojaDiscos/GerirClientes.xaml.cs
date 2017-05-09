9959c9b52a6b32dc8f77904d7c55b9c85b57863f
Filipe <Filipe@DESKTOP-M0EEND0>
2017-05-09 17:47:48 +0100

    gerir clientes

--- LojaDiscos/GerirClientes.xaml.cs
+++ LojaDiscos/GerirClientes.xaml.cs
@@ -30,5 +30,26 @@ namespace LojaDiscos
             CriarFichaCliente criarFichaCliente = new CriarFichaCliente();
             this.NavigationService.Navigate(criarFichaCliente);
         }
+
+        private void listView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ListView listView = sender as ListView;
+            GridView gView = listView.View as GridView;
+
+            var workingWidth = listView.ActualWidth - SystemParameters.VerticalScrollBarWidth; // take into account vertical scrollbar
+            var col1 = 0.1;
+            var col2 = 0.3;
+            var col3 = 0.3;
+            var col4 = 0.1;
+            var col5 = 0.1;
+            var col6 = 0.1;
+
+            gView.Columns[0].Width = workingWidth * col1;
+            gView.Columns[1].Width = workingWidth * col2;
+            gView.Columns[2].Width = workingWidth * col3;
+            gView.Columns[3].Width = workingWidth * col4;
+            gView.Columns[4].Width = workingWidth * col5;
+            gView.Columns[5].Width = workingWidth * col6;
+        }
     }
 }
