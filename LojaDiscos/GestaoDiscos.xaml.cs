4254aaf860e9920245e5d4be666f0f45a1129dff
Filipe <Filipe@DESKTOP-M0EEND0>
2017-05-10 01:14:28 +0100

    gestao de discos: inserir dinamicamente elementos

--- LojaDiscos/GestaoDiscos.xaml.cs
+++ LojaDiscos/GestaoDiscos.xaml.cs
@@ -29,11 +29,46 @@ namespace LojaDiscos
 
         public void carregarCds()
         {
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < 3; i++)
             {
+                StackPanel sp = new StackPanel();
+                sp.Orientation = Orientation.Vertical;
+
+                Image Img = new Image();
+                Img.Source = new BitmapImage(new Uri("C:/Users/Filipe/Desktop/UA/3ano/ihc/projeto/LojaDiscos/LojaDiscos/Images/icon.png"));
+                //Image resizedImage = new Bitmap(image, new Size(50, 50));
+                TextBlock text = new TextBlock();
+                text.Text = "TEste " + i + " !";
+
+                sp.Children.Add(Img);
+                sp.Children.Add(text);
+
+                this.wrapPanelDiscos.Children.Add(sp);
+            }
+            
+        }
+
+
+    }
+
+}
+
+
+/*using (SqlConnection sc = ConnectionHelper.GetConnection())
+            {
+                sc.Open();
+                string sql = "Select * FROM Pessoa As P JOIN Cliente As C ON P.nif = C.nif";
+                SqlCommand com = new SqlCommand(sql, sc);
+            
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(com))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGrid.ItemsSource = dt.DefaultView;
+                }
+
+
 
             }
-            this.wrapPanelDiscos
-        }
-    }
-}
+*/
